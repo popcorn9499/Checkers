4d94fbc1a829f01d64d323806d92c80b58216d88
popcorn9499 <popcorn9499@gmail.com>
2018-01-21 11:43:35 -0600

    fixing jump in some cases
    Mixed up the x and y somewhere in the list which caused issues

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ def isValidJump(board,x1,y1,x2,y2):
 	print("X1: {0} Y1: {1} X2: {2} Y2: {3}".format(x1,y1,x2,y2))
 	if x2 > 7 or x2 < 0 or y2 > 7 or y2 < 0:
 		return False
-	if board[x1][y1]["Type"] == "Men" and board[int(abs((y1+y2)/2))][int(abs((x1+x2)/2))]["Type"] == "Men":
+	if board[y1][x1]["Type"] == "Men" and board[int(abs((y1+y2)/2))][int(abs((x1+x2)/2))]["Type"] == "Men":
 		
 		if abs(x1-x2) != 2 or abs(y1-y2) != 2: #checks if the move wanted is the right amount of spaces away or not
 			print("Not enough spaces")
